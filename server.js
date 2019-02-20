5d09d3aab8ea6865f04098fe54f74f5f5bb7ebfa
Paulius Kutka <kutka100@gmail.com>
2019-02-20 14:39:01 +0200

    Added server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var proxy = require("http-proxy-middleware");
+var API_HOST = process.env.API_HOST || "localhost:8080";
+var PORT = process.env.PORT || 8080;
+
+var buildPath = "dist/good-works";
+
+// Initialize
+var app = express();
+
+// Serve static resources from 'build' folder
+app.use(express.static(buildPath));
+
+// // Enable proxy to api
+// app.use('/api', proxy({
+//     target: API_HOST,
+//     changeOrigin: true,
+//     pathRewrite: {
+//         '^/api': ''
+//     }
+// }));
+
+// Otherwise serve index.html
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + buildPath + "/index.html");
+});
+
+app.listen(PORT);
